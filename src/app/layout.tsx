3257f77a37f295e77e4708687d25b5514ae76a67
pham_duc <ducpv@twendeesoft.com>
2024-04-06 11:50:52 +0700

    build: update-logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Evocean",
+  title: "MoonKit",
   description: "NFT Digital Assets Marketplace",
   icons: [
     {
