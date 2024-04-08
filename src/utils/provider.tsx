cd91af912eac2e3e7d0f5e757c3db9985eb5471f
pham_duc <ducpv@twendeesoft.com>
2024-04-08 16:03:56 +0700

    fix: toast-noti

--- src/utils/provider.tsx
+++ src/utils/provider.tsx
@@ -10,7 +10,8 @@ import { clusterApiUrl } from "@solana/web3.js";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FC, PropsWithChildren } from "react";
 import { ToastContainer, Bounce } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+// import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.min.css';
 
 const client = new QueryClient();
 const Providers: FC<PropsWithChildren> = ({ children }) => {
